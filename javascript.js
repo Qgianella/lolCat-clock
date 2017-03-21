c2dfe3372f459b15e5a3e6ca81bd0c6ee59a993d
QGianella <gianella7@gmail.com>
2017-03-20 22:29:53 -0700

    linking js images

--- javascript.js
+++ javascript.js
@@ -8,20 +8,30 @@ var napTime = lunchTime + 1; // 3pm
 var messageText;
 var timeEventJS = document.getElementById("timeEvent");
 
+//getting image element
+var lolcatImage = document.getElementById('lolcat');
+//defining default image
+var image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/normalTime.jpg";
+
 if (time < noon) {
   messageText = "Good morning";
 } else if (time > evening) {
   messageText = "Good evening";
 } else if (time == partyTime) {
   messageText = "Happy hour anyone?";
+  image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat4.jpg";
 } else if (time == lunchTime) {
   messageText = "It's time to eat!";
+  image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat2.jpg";
 } else if (time == wakeUpTime) {
   messageText = "Wakey, wakey..It's time to get up";
+  image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat1.jpg";
 } else if (time == napTime) {
   messageText = "zzzz...it's nap time"
+  image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat3.jpg";
 } else {
   messageText = "Good afternoon";
 }
 
-timeEventJS.innerText=messageText;
+timeEventJS.innerText = messageText;
+lolcat.src = image;
