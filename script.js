edf9177faa657427c7bd424c9f2b5935424e069a
QGianella <gianella7@gmail.com>
2017-03-21 21:16:57 -0700

    adding a clock

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-var time = new Date().getHours();
 var noon = 12;
 var evening = 19; //7pm
 var partyTime = 17; //5pm
 var lunchTime = 13; //1pm
 var wakeUpTime = 7; //7am
 var napTime = lunchTime + 1; // 3pm
+
+//updateClock wrapping everything will show seconds on clock:
+var updateClock = function()
+{
+var time = new Date().getHours();
 var messageText;
 var timeEventJS = document.getElementById("timeEvent");
 
@@ -35,3 +39,60 @@ if (time < noon) {
 
 timeEventJS.innerText = messageText;
 lolcat.src = image;
+
+
+//clock:
+
+var showCurrentTime = function()
+{
+    // display the string on the webpage
+    var clock = document.getElementById('clock');
+
+    var currentTime = new Date();
+
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+    var meridian = "AM";
+
+    // Set hours
+    if (hours >= noon)
+    {
+        meridian = "PM";
+    }
+    if (hours > noon)
+    {
+        hours = hours - 12;
+    }
+
+    // Set Minutes
+    if (minutes < 10)
+    {
+        minutes = "0" + minutes;
+    }
+
+    // Set Seconds
+    if (seconds < 10)
+    {
+        seconds = "0" + seconds;
+    }
+
+    // put together the string that displays the time
+    var clockTime = hours + ':' + minutes + ':' + seconds + " " + meridian + "!";
+
+    clock.innerText = clockTime;
+};
+
+
+//call the showCurrentTime fuction:
+showCurrentTime();
+//end of UpdateClock  defined function
+  };
+
+//call updateClock function:
+updateClock();
+
+//increment the clock once a second
+
+var oneSecond = 1000;
+setInterval( updateClock, oneSecond);
